685588289ad4756ffbf4c48c2b6f380807cbffae
andot <mabingyao@gmail.com>
2018-03-23 11:25:01 +0800

    Update Out-StripAnsi.psm1

--- Out-StripAnsi.psm1
+++ Out-StripAnsi.psm1
@@ -15,17 +15,18 @@ function Out-StripAnsi {
         $InputObject
     )
     BEGIN {
-        $Pattern = "[\u001B\u009B][[\]()#;?]*(?:(?:(?:[a-zA-Z\d]*(?:;[a-zA-Z\d]*)*)?\u0007)|(?:(?:\d{1,4}(?:;\d{0,4})*)?[\dA-PRZcf-ntqry=><~]))";
+        $Pattern = "[\u001B\u009B][[\]()#;?]*(?:(?:(?:[a-zA-Z\d]*(?:;[a-zA-Z\d]*)*)?\u0007)|(?:(?:\d{1,4}(?:;\d{0,4})*)?[\dA-PR-TZcf-ntqry=><~]))";
     }
     PROCESS {
         if ($InputObject -isnot [string]) {
             $InputObject = $InputObject.ToString();
         }
         if ($InputObject.Length -eq 0) {
-            Write-Output ""
-            return;
+            ""
         }
-        $InputObject -replace $Pattern, ""
+        else {
+            $InputObject -replace $Pattern, ""
+        }
     }
     END {
     }
